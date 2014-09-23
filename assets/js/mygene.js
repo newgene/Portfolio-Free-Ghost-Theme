afd68249093468fdd5003384854a1b2d8776576d
Chunlei Wu <anewgene@yahoo.com>
2014-09-23 22:39:38 +0000

    adding mygene.js

---
+++ assets/js/mygene.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+    $("#learnmore__btn_id").magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+        fixedContentPos: false
+    });    
+});
